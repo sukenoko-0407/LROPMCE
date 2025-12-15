b23040cd6d3f366f08c3caa5ebb61a948c938c4e
sukenoko-0407 <takahiro.kimoto.0407@gmail.com>
2025-12-15 19:51:39 +0900

    Delete ecmporl directory

--- ecmporl/mcts/path.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Tuple
-from ..types import BranchKey, LeafKey
-
-@dataclass(frozen=True)
-class PathToken:
-    # Tuple of (branch_key, action_id)
-    edges: Tuple[Tuple[BranchKey, int], ...]
-    leaf_key: LeafKey
--- ecmporl/types.py
+++
@@ -1,5 +0,0 @@
-from typing import Tuple
-
-# (canonical_smiles, depth_action)
-LeafKey = Tuple[str, int]
-BranchKey = Tuple[str, int]
